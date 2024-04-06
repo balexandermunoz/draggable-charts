c4a19c80d1212f5fe9032fca699c3ff90ecdeffb
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-04-05 22:23:42 -0500

    :art: formated code

--- my_component/frontend/src/MyComponent.jsx
+++ my_component/frontend/src/MyComponent.jsx
@@ -1,35 +1,37 @@
-import React from "react";
-import { Chart, registerables } from "chart.js";
-import { getRelativePosition } from 'chart.js/helpers';
-import { Line, getElementAtEvent } from "react-chartjs-2";
+import React from "react"
+import { Chart, registerables } from "chart.js"
+import { getRelativePosition } from "chart.js/helpers"
+import { Line, getElementAtEvent } from "react-chartjs-2"
 import {
   Streamlit,
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
 
-Chart.register(...registerables);
+Chart.register(...registerables)
 
 class MyChartComponent extends StreamlitComponentBase {
   constructor(props) {
-    super(props);
+    super(props)
     console.log(props.args)
-    this.chartRef = React.createRef();
+    this.chartRef = React.createRef()
 
-    const datasets = Object.entries(props.args.data).map(([colName, colData], index) => {
-      const data = Object.values(colData);
-      return {
-        data,
-        label: colName,
-        fill: false,
-        lineTension: 0.3,
-        cubicInterpolationMode: 'monotone'
-      };
-    });
+    const datasets = Object.entries(props.args.data).map(
+      ([colName, colData], index) => {
+        const data = Object.values(colData)
+        return {
+          data,
+          label: colName,
+          fill: false,
+          lineTension: 0.3,
+          cubicInterpolationMode: "monotone",
+        }
+      }
+    )
     if (props.args.colors) {
       datasets.forEach((dataset, index) => {
-        dataset.borderColor = props.args.colors[index];
-      });
+        dataset.borderColor = props.args.colors[index]
+      })
     }
     this.state = {
       activePoint: null,
@@ -37,7 +39,7 @@ class MyChartComponent extends StreamlitComponentBase {
         labels: Object.keys(props.args.data.Col1),
         datasets: datasets,
       },
-    };
+    }
   }
 
   options = {
@@ -50,78 +52,82 @@ class MyChartComponent extends StreamlitComponentBase {
     },
     onHover: (event, chartElement) => {
       if (event && event.target) {
-        event.target.style.cursor = chartElement[0] ? "pointer" : "default";
+        event.target.style.cursor = chartElement[0] ? "pointer" : "default"
       }
     },
     plugins: {
       title: {
-         display: true,
-         text: this.props.args.title
-       },
+        display: true,
+        text: this.props.args.title,
+      },
     },
-  };
+  }
 
   downHandler = (event) => {
     const points = getElementAtEvent(this.chartRef.current, event, {
       intersect: false,
-    });
+    })
     if (points.length > 0) {
-      this.setState({ activePoint: points[0] });
+      this.setState({ activePoint: points[0] })
     }
-  };
+  }
 
   upHandler = (event) => {
-    if (this.state.activePoint){
-      const data = this.chartRef.current.data.datasets.reduce((acc, dataset) => {
-        const colData = dataset.data.reduce((colAcc, value, index) => {
-          colAcc[this.state.chartData.labels[index]] = value;
-          return colAcc;
-        }, {});
-        acc[dataset.label] = colData;
-        return acc;
-      }, {});
-  
-      Streamlit.setComponentValue(data);
-      console.log(data);
+    if (this.state.activePoint) {
+      const data = this.chartRef.current.data.datasets.reduce(
+        (acc, dataset) => {
+          const colData = dataset.data.reduce((colAcc, value, index) => {
+            colAcc[this.state.chartData.labels[index]] = value
+            return colAcc
+          }, {})
+          acc[dataset.label] = colData
+          return acc
+        },
+        {}
+      )
+
+      Streamlit.setComponentValue(data)
+      console.log(data)
     }
-    this.setState({ activePoint: null });
-  };
+    this.setState({ activePoint: null })
+  }
 
   moveHandler = (event) => {
     if (this.state.activePoint) {
-      const chart = this.chartRef.current;
-      const position = getRelativePosition(event, this.chartRef.current);
-      const chartArea = chart.chartArea;
-      const yAxis = chart.scales.y;
+      const chart = this.chartRef.current
+      const position = getRelativePosition(event, this.chartRef.current)
+      const chartArea = chart.chartArea
+      const yAxis = chart.scales.y
       const yValue = this.map(
         position.y,
         chartArea.bottom,
         chartArea.top,
         yAxis.min,
         yAxis.max
-      );
-      chart.data.datasets[this.state.activePoint.datasetIndex].data[this.state.activePoint.index] =
-        yValue;
-      chart.update();
+      )
+      chart.data.datasets[this.state.activePoint.datasetIndex].data[
+        this.state.activePoint.index
+      ] = yValue
+      chart.update()
     }
-  };
+  }
 
   map = (value, start1, stop1, start2, stop2) => {
-    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-  };
+    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1))
+  }
 
   render() {
     return (
-        <Line
-          ref={this.chartRef}
-          data={this.state.chartData}
-          options={this.options}
-          onPointerDown={this.downHandler}
-          onPointerUp={this.upHandler}
-          onPointerMove={this.moveHandler}
-        />
-    );
+      <Line
+        ref={this.chartRef}
+        data={this.state.chartData}
+        options={this.options}
+        onPointerDown={this.downHandler}
+        onPointerUp={this.upHandler}
+        onPointerMove={this.moveHandler}
+      />
+    )
   }
 }
 
-export default withStreamlitConnection(MyChartComponent);
+export default withStreamlitConnection(MyChartComponent)
