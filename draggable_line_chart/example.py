9eceb7cc82734b7feddaf74b5be09cfb9697f433
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-04-07 15:35:38 -0500

    :memo: Update example

--- draggable_line_chart/example.py
+++ draggable_line_chart/example.py
@@ -1,13 +1,22 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+
 from draggable_line_chart import draggable_line_chart
 
-st.subheader("Draggable Plot!")
+st.header("Draggable Plot!")
+
 initial_data = pd.DataFrame({
     "Col1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    "Col2": [1, 4, 9, 16, 25, 36, 49, 64, 81, 100],
-    "Col3": [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-}).to_dict()
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c']
-new_data = draggable_line_chart("My Plot", initial_data, colors=colors, key="foo")
+    "Col2": [1, 4, 9, 16, 25, 36, 49, 64, 81, 200],
+    "Col3": [-1, -2, -3, -4, -5, -6, -7, -8, -50, -100]
+})
+
+
+plot_options = {
+    "title": "My Plot",
+    "colors": ['#1f77b4', '#ff7f0e', '#2ca02c'],
+    "x_label": "X Axis",
+    "y_label": "Y Axis",
+}
+new_data = draggable_line_chart(data=initial_data.to_dict(), options=plot_options)
 new_data
