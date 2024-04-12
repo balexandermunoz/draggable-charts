c6aaa02537623fd969b19ea2df28a5f6e95f708d
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-04-12 16:56:29 -0500

    :ambulance: Fix undefined tension option

--- draggable_line_chart/frontend/src/chartData.jsx
+++ draggable_line_chart/frontend/src/chartData.jsx
@@ -1,7 +1,6 @@
 export function createChartData(data, options) {
-  console.log(options)
   const tension =
-    options.tension !== undefined
+    options && options.tension !== undefined
       ? Math.min(Math.max(options.tension, 0), 0.4)
       : 0.3
   const datasets = Object.entries(data).map(([colName, colData], index) => {
