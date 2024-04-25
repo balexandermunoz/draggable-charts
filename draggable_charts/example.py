266fa1b7cde978ae61a80ad937720ef866331380
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-04-25 16:50:08 -0500

    :art: Format document

--- draggable_charts/example.py
+++ draggable_charts/example.py
@@ -71,7 +71,8 @@ scatter_data = {
 }
 new_scatter_data = scatter_chart(
     data=scatter_data,
-    options={"x_labels": x_cat, "show_line": True, "tension": 0, "fixed_lines": ["trace 1"]}
+    options={"x_labels": x_cat, "show_line": True,
+             "tension": 0, "fixed_lines": ["trace 1"]}
 )
 # new_scatter_data
 
@@ -82,4 +83,4 @@ data = {
     "trace 2": {"x": [1, 2, 3, 4, 5], "y": [1, 8, 27, 64, 125]},
 }
 new_data = bezier_chart(data, t=0.5, options={"fixed_lines": ["trace 2"]})
-#new_data
+# new_data
