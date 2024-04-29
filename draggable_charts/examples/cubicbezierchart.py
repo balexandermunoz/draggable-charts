97aee9f804f4a6ae69f35c6860382c60b31b46ef
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-04-28 23:34:36 -0500

    :memo: Add cubic bezier chart example

--- draggable_charts/examples/cubicbezierchart.py
+++ draggable_charts/examples/cubicbezierchart.py
@@ -4,7 +4,7 @@ from draggable_charts import cubic_bezier_chart
 
 st.header("Cubic Bezier")
 data = {
-    "trace 1": {"x": [1, 2, 3, 4], "y": [-1, -5, 10, -2]},
+    "trace 1": {"x": [1, 2, 3, 4], "y": [-1, -5, 10, -10]},
     "trace 2": {"x": [1, 2, 3, 4], "y": [1, 8, 27, 5]},
 }
 new_data = cubic_bezier_chart(
