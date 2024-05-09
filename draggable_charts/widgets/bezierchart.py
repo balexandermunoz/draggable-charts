92c268e87e942884863a6d4b8dee931455f9b747
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-05-08 21:30:44 -0500

    :fire: Remove interpolation logic
    Remove calculate control points logic and include bezier_interpolation library to handle it

--- draggable_charts/widgets/bezierchart.py
+++ draggable_charts/widgets/bezierchart.py
@@ -1,5 +1,7 @@
 from typing import Any, Callable
 
+from bezier_interpolation import quadratic_interpolation
+
 from ..utils import component, get_func_name, register
 from ..utils.data_validation import validate_bezier_data
 from ..utils.options import set_options
@@ -22,48 +24,12 @@ def bezier_chart(
     return component(id=get_func_name(), kw=locals(), default=default_data, key=key)
 
 
-def add_control_points(data: dict, options: dict, t: float = 0.5) -> dict:
+def add_control_points(data: dict, options: dict, t: float) -> dict:
     for trace_name, trace_data in data.items():
-        if len(trace_data['x']) < 2 or trace_name in options["fixed_lines"]:
+        if trace_name in options["fixed_lines"]:
             continue
-        control_points = calculate_control_points(trace_data, t)
-        # Add control points into data:
-        for i, (x_c, y_c) in enumerate(control_points):
-            trace_data['x'].insert(2 * i + 1, x_c)
-            trace_data['y'].insert(2 * i + 1, y_c)
+        
+        points = list(zip(trace_data['x'], trace_data['y']))
+        new_points = quadratic_interpolation(points, t)
+        trace_data['x'], trace_data['y'] = zip(*new_points)
     return data
-
-
-def calculate_control_points(trace_data: dict, t: float) -> list:
-    control_points = []
-    for i in range(len(trace_data['x']) - 1):
-        x_0, y_0 = trace_data['x'][i], trace_data['y'][i]
-        x_1, y_1 = trace_data['x'][i + 1], trace_data['y'][i + 1]
-        if i == 0:
-            x_c, y_c = calculate_first_control_point(x_0, y_0, x_1, y_1, t)
-        else:
-            x_c, y_c = calculate_next_control_point(x_c, y_c, x_0, y_0, x_1)
-        control_points.append((x_c, y_c))
-    return control_points
-
-
-def calculate_first_control_point(
-    x_0: float, y_0: float,
-    x_1: float, y_1: float,
-    t: float
-) -> tuple[float, float]:
-    x_c = x_0 + (x_1 - x_0) / 2
-    y_c = y_0 + t * (y_1 - y_0)
-    return x_c, y_c
-
-
-def calculate_next_control_point(
-    x_c: float, y_c: float,
-    x_0: float, y_0: float,
-    x_1: float
-) -> tuple[float, float]:
-    m = (y_0 - y_c) / (x_0 - x_c)
-    b = y_0 - m * x_0
-    x_cn = x_0 + (x_1 - x_0) / 2
-    y_cn = m * x_cn + b
-    return x_cn, y_cn
--- draggable_charts/widgets/cubicbezierchart.py
+++ draggable_charts/widgets/cubicbezierchart.py
@@ -1,6 +1,6 @@
 from typing import Any, Callable
 
-import numpy as np
+from bezier_interpolation import cubic_interpolation
 
 from ..utils import component, get_func_name, register
 from ..utils.data_validation import validate_bezier_data
@@ -28,70 +28,7 @@ def add_control_points(data: dict, options: dict) -> dict:
         if trace_name in options["fixed_lines"]:
             continue
 
-        new_trace_data = {'x': [], 'y': []}
-        if len(trace_data['x']) == 2:
-            # If there are only two points, add two intermediate points
-            x1, x2 = trace_data['x']
-            y1, y2 = trace_data['y']
-            new_trace_data['x'] = [x1, x1 + (x2 - x1) / 3, x1 + 2 * (x2 - x1) / 3, x2]
-            new_trace_data['y'] = [y1, y1 + (y2 - y1) / 3, y1 + 2 * (y2 - y1) / 3, y2]
-
-        else:
-            P1, P2 = calculate_control_points(trace_data)
-            for i in range(len(trace_data['x']) - 1):
-                new_trace_data['x'].append(trace_data['x'][i])
-                new_trace_data['x'].append(P1[i][0])
-                new_trace_data['x'].append(P2[i][0])
-
-                new_trace_data['y'].append(trace_data['y'][i])
-                new_trace_data['y'].append(P1[i][1])
-                new_trace_data['y'].append(P2[i][1])
-            new_trace_data['x'].append(trace_data['x'][-1])
-            new_trace_data['y'].append(trace_data['y'][-1])
-        data[trace_name] = new_trace_data
+        points = list(zip(trace_data['x'], trace_data['y']))
+        new_points = cubic_interpolation(points)
+        trace_data['x'], trace_data['y'] = zip(*new_points)
     return data
-
-
-def calculate_control_points(trace_data: dict) -> list:
-    """See:
-    https://www.particleincell.com/2012/bezier-splines/
-    Second last eq has a typo, it's P2_i = 2*K_(i+1) - P1_(i+1)
-    """
-    n = len(trace_data['x']) - 1  # N. of segments
-
-    # Create matrix A (Ax + b = 0)
-    A = create_tri_diagonal_matrix(n)
-
-    # Create vector b (each component contains bx and by)
-    K = np.array(list(zip(trace_data['x'], trace_data['y'])))
-
-    b = np.array(
-        [(K[0][0] + 2 * K[1][0], K[0][1] + 2 * K[1][1])]
-        + [(4 * K[i][0] + 2 * K[i+1][0], 4 * K[i][1] + 2 * K[i+1][1]) for i in range(1, n - 1)]
-        + [(8 * K[n-1][0] + K[n][0], 8 * K[n-1][1] + K[n][1])]
-    )
-
-    # Solve the system:
-    P1 = np.linalg.solve(A, b)
-
-    # Compute P2:
-    # P2_i = ( 2*K_(i+1) - P1_(i+1) ),  for i = 0, ..., n-1
-    # P2_(n-1) = (1/2) * (K_(n-1) + P1_(n-1) )
-    P2 = 2 * K[1:-1] - P1[1:]
-    P2 = np.concatenate((P2, [(1/2)*(K[-1] + P1[-1])]))
-    return P1, P2
-
-
-def create_tri_diagonal_matrix(n):
-    """This function creates the A matrix to find the control points of the cubic bezier curve.
-    See "The Matrix" Section in:
-    https://exploringswift.com/blog/Drawing-Smooth-Cubic-Bezier-Curve-through-prescribed-points-using-Swift
-    """
-    A = np.zeros((n, n))
-    # Main diagonal
-    A += np.diag([2] + [4]*(n-2) + [7], k=0)
-    # Upper diagonal
-    A += np.diag([1]*(n-1), k=1)
-    # Lower diagonal
-    A += np.diag([1]*(n-2) + [2], k=-1)
-    return A
