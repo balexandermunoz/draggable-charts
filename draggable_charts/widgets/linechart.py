76c6345b8e5b292d0f98ecd6f9220d5133fe8db0
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-05-09 16:20:54 -0500

    :memo: Update linechart docstring

--- draggable_charts/widgets/linechart.py
+++ draggable_charts/widgets/linechart.py
@@ -25,20 +25,13 @@ def line_chart(
         The data to display in the chart. Index is always X values and columns are Y values. Columns should have only numeric values. 
         Series.name is the trace name. If a DataFrame is provided, the column names are the trace names.
     options : dict[str: any], optional
-        A dictionary of options for the chart. It can include the following keys:
-        - 'title': The title of the chart.
-        - 'colors': A list of colors for the chart traces. Each color must be a hexadecimal color code. The order of the colors corresponds to the order of the columns.
-        - 'x_label': The label for the x-axis.
-        - 'y_label': The label for the y-axis.
-        - 'x_grid': A boolean indicating whether to display the grid for the x-axis.
-        - 'y_grid': A boolean indicating whether to display the grid for the y-axis.
-        - 'legend': A boolean indicating whether to display the legend. If not provided, the legend will be displayed by default.
-        - 'legend_position': The position of the legend. It can be 'top', 'left', 'bottom', or 'right'.
-        - 'legend_align': The alignment of the legend. It can be 'start', 'center', or 'end'.
-        - 'tension': The tension of the lines. 0 gives straight lines, 0.5 gives smooth lines. Default is 0.3.
-        - 'fill_gaps': A Boolean that indicates whether NaN values are filled in the lines. If False, lines will be broken at NaN values. Default is False.
-        - 'fixed_lines': List of column names that cannot be dragged. Default is an empty list.
-        If not provided, default options will be used.
+        A dictionary of options for the chart.
+    on_change : Callable, optional
+        A callback function that is called with the new data of the chart after user interaction.
+    args : tuple, optional
+        The arguments to pass to the callback function.
+    kwargs : dict, optional
+        The keyword arguments to pass to the callback function.
     key : str, optional
         An optional string to use as the unique key for the widget. If this is None, and the component's arguments are changed, the component will be re-mounted in the Streamlit frontend and lose its current state.
 
